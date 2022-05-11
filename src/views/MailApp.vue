37389bb0c63c6437ca8e325d979db0d51a460ad9
qyb <qiuyingbo@gmail.com>
2022-05-11 17:20:28 +0800

    定义 mailapp 三栏样式; 以及解决 @ 路径问题

--- src/views/MailApp.vue
+++ src/views/MailApp.vue
@@ -1,13 +1,23 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import { onMounted } from 'vue'
+import { getClientHeight, getClientWidth } from '@/layout';
 
 const width = ref(0)
 const height = ref(0)
+const folderClass = ref('folder-1')
+const msglistClass = ref('msglist-1')
 
 function onResize (ele: HTMLElement): void {
   width.value = ele.offsetWidth,
   height.value = ele.offsetHeight
 }
+
+onMounted(() => {
+  const h = getClientHeight()
+  const w = getClientWidth()
+  console.log(h, w)
+})
 
 defineProps<{
   arg: string
@@ -17,12 +27,62 @@ defineProps<{
 
 <template>
   <div v-layout:[arg]="onResize" class="appcontainer">
-    foobar, {{ width }}, {{ height }}
+    <div class="menu">
+      foobar, {{ width }}, {{ height }}
+    </div>
+    <div class="main">
+      <div :class="folderClass" class="folder">
+      </div>
+      <div :class="msglistClass" class="msglist">
+
+      </div>
+      <div class="msgcontent"></div>
+    </div>
   </div>
 </template>
 
 <style>
 .appcontainer {
-  height: 100%
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+
+.main {
+  flex-grow: 1;
+  display: flex;
+  flex-direction: row;
+}
+
+.folder {
+  background-color: aquamarine;
+}
+
+.folder-1 {
+  width: 198px;
+  max-width: 198px;
+}
+
+.folder-2 {
+  width: 214px;
+  max-width: 236px;
+}
+
+.msglist {
+  background-color: beige;
+}
+
+.msglist-1 {
+  width: 350px;
+  max-width: 350px;
+}
+
+.msglist-2 {
+  width: 376px;
+  max-width: 380px;
+}
+
+.msgcontent {
+  flex-grow: 1;
 }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+        "@": resolve(__dirname, 'src'), // 路径别名
+    },
+    extensions: ['.js', '.json', '.ts'] // 使用路径别名时想要省略的后缀名，可以自己 增减
+  },
   plugins: [vue()]
 })
