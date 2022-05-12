eefd310f9171f788e023cf3d51cccc01e4d0760e
qyb <qiuyingbo@gmail.com>
2022-05-12 09:36:58 +0800

    之前手滑了，定义了 resizeThrottler 然而并没有调用它

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script lang="ts">
-//import HelloWorld from './components/HelloWorld.vue'
-//import block from './components/block.vue'
-//import BlockTwo from './components/BlockTwo.vue'
 import MailApp from './views/MailApp.vue'
 import { defineComponent } from 'vue'
+
+// resize 事件处理有性能损失，需要想办法做一个限流阀 resizeThrottler
+// https://developer.mozilla.org/zh-CN/docs/Web/API/Window/resize_event
+let resizeTimeout: number | null | NodeJS.Timeout = null
 
 export default defineComponent({
   name: 'App',
@@ -24,30 +25,29 @@ export default defineComponent({
         this.$layout[key].handler(ele)
       }
     },
+
+
+    resizeThrottler (): void {
+      // ignore resize events as long as an actualResizeHandler execution is in the queue
+      if ( !resizeTimeout ) {
+        resizeTimeout = setTimeout(() => {
+          resizeTimeout = null
+          // The actualResizeHandler will execute at a rate of 15fps
+          this.onResize()
+        }, 66)
+      }
+    }
   },
 
   mounted () {
     this.onResize()
 
-    // 这里还是有性能损失，需要想办法做一个限流阀
-    // https://developer.mozilla.org/zh-CN/docs/Web/API/Window/resize_event
-    let resizeTimeout: number | null | NodeJS.Timeout = null
-    const _this = this
-    function resizeThrottler () {
-      // ignore resize events as long as an actualResizeHandler execution is in the queue
-      if ( !resizeTimeout ) {
-        resizeTimeout = setTimeout(function() {
-          resizeTimeout = null
-          // The actualResizeHandler will execute at a rate of 15fps
-          _this.onResize()
-        }, 66)
-      }
-    }
-    window.addEventListener('resize', this.onResize, { passive: true })
+
+    window.addEventListener('resize', this.resizeThrottler, { passive: true })
   },
 
   unmounted () {
-    window.removeEventListener('resize', this.onResize)
+    window.removeEventListener('resize', this.resizeThrottler)
   }
 })
 
