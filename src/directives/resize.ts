866de4de7ff1fb9565a4cd391907480937c10076
qyb <qiuyingbo@gmail.com>
2022-05-16 09:36:59 +0800

    add comments

--- src/directives/resize.ts
+++ src/directives/resize.ts
@@ -1,6 +1,11 @@
 import type { DirectiveBinding, App } from 'vue'
 import { $globalLayout } from '@/utils/global'
 
+/*
+  所有调用 v-layout 的 HTMLElement 都必须传入 arg 参数，作用是：
+    1. 插入 $globalLayout Object 以在 App.vue 处理 resize event 时遍历
+    2. unmount 时可以从 Object 中删除
+ */
 function mounted (el: HTMLElement, binding: DirectiveBinding) {
   const handler = binding.value
   const key = binding.arg
